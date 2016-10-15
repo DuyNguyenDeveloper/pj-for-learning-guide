acdef8488ad009f13ab1e47150296c3e5e938a76
nguye <nguye@EDOGAWA-CONAN>
2016-10-16 00:54:13 +0700

    Add project files.

---
+++ SnakeGame/Main.cpp
@@ -0,0 +1,165 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score; // Biến toàn cục
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection { STOP = 0, UP, LEFT, RIGHT, DOWN }; // Hướng đi
+eDirection dir;
+void Setup()
+{
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width; // Tạo ra đồ ăn cho rắn
+	fruitY = rand() % height;
+	score = 0; // Cho điểm mặc định là 0 
+}
+void Draw()
+{
+	system("cls"); // system("clear");
+				   //Tạo khung cho rắn chạy
+	for (int i = 0; i < width + 2; i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0)
+				cout << "#";
+
+			if (i == y && j == x)
+				cout << "O";
+			else if (i == fruitY && j == fruitX)
+				cout << "o";
+			else
+			{
+				bool print = false;
+				for (int k = 0; k < nTail; k++)
+				{
+					if (tailX[k] == j && tailY[k] == i)
+					{
+						cout << "o";
+						print = true;
+					}
+				}
+				if(!print)
+					cout << " ";
+			}
+
+			if (j == width - 1)
+				cout << "#";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < width + 2; i++)
+		cout << "#";
+	cout << endl;
+	cout << "Score : " << score << endl;
+}
+void Input()
+{
+	//hàm keyboard hit ( nhận 1 ký tự trong vùng đệm bàn phím )
+	if (_kbhit())
+	{
+		//Tạo control điều khiển rắn
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
+}
+void Logic()
+{
+	//Tạo thân mình con rắn
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++)
+	{
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+
+	}
+	switch (dir)
+	{
+	case STOP:
+		break;
+	case UP:
+		y--;
+		break;
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case DOWN:
+		y++;
+		break;	
+	default:
+		break;
+	}
+	//Nếu con rắn đụng tường sẽ dừng lại
+	/*if (x > width || x<0 || y>height || y < 0)
+	{
+		gameOver = true;
+	}*/
+
+	//Nếu con rắn từ 20 điểm trở lên sẽ không bị dừng khi đụng tường
+	if (x >= width) x = 0; else if (x < 0) x = width - 1;
+	if (y >= height) y = 0; else if (y < 0) y = height - 1;
+	for (int i = 0; i < nTail; i++)
+	{
+		if (tailX[i] == x && tailY[i] == y)
+			gameOver = true;
+	}
+	//Nếu đụng trúng đồ ăn sẽ tăng điểm lên
+	if (x == fruitX && y == fruitY)
+	{
+		score += 10;
+		//Tạo đồ ăn mới
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		nTail++;
+
+	}
+}
+int main()
+{
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		//Sleep(10); sleep(10);
+	}
+	return 0;
+}
---
+++ SnakeGame_ver2/Main.cpp
@@ -0,0 +1,169 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "console.h"
+#include <conio.h>
+#include <time.h>
+using namespace std;
+enum eDirection{UP,DOWN,LEFT,RIGHT,STOP=0};
+const int width = 25;
+const int height = 25;
+bool gameOver;
+//Tọa độ x,y
+struct Coordinates
+{
+	int x, y;
+};
+struct Fruit
+{
+	Coordinates fruit;
+};
+struct Snake
+{
+	// đốt rắn
+	Coordinates dot[20];
+	// rắn có n đốt
+	int n;
+	//Điều hướng
+	eDirection dir;
+
+};
+void Initialize(Snake &snake,Fruit &fr)
+{
+	gameOver = false;
+	//Rắn có 1 cái đầu đầu tiên
+	snake.n = 1;
+	snake.dot[0].x = 0;
+	snake.dot[0].y = 0;
+	snake.dir = RIGHT;
+	
+	fr.fruit.x = 10;
+	fr.fruit.y = 10;
+}
+void Show(Snake snake,Fruit fr)
+{
+	system("cls");
+	
+	//In màu mặc định tường
+	TextColor(default_ColorCode);
+	for (int i = 0; i < width; i++)
+	{
+		gotoXY(i, height);
+		putchar(196);
+	}
+	TextColor(default_ColorCode);
+	for (int i = 0; i < height; i++)
+	{
+		gotoXY(width,i);
+		putchar(179);
+	}
+
+	//Rắn ăn hoa quả
+	TextColor(ColorCode_Blue);
+	gotoXY(fr.fruit.x, fr.fruit.y);
+	putchar('@');
+
+	TextColor(ColorCode_Green);
+	for (int i = 0; i < snake.n; i++)
+	{
+		gotoXY(snake.dot[i].x, snake.dot[i].y);
+		putchar('*');
+	}
+}
+void Direction(Snake &snake)
+{
+	//Cái thân theo sau cái đầu
+	for (int i = snake.n - 1; i > 0; i--)
+	{
+		snake.dot[i] = snake.dot[i-1];
+	}
+	//Hàm phát hiện phím bất kỳ trên bàn phím
+	if (_kbhit())
+	{
+		int key = _getch(); // lấy mã 
+		//Điều khiển cái đầu
+		if (key == 'a' || key == 'A')
+			snake.dir = LEFT;
+		else if (key == 'd' || key == 'D')
+			snake.dir = RIGHT;
+		else if (key == 'w' || key == 'W')
+			snake.dir = UP;
+		else if (key == 's' || key == 'S')
+			snake.dir = DOWN;
+	}
+	//Theo tọa độ x,y hướng lên giảm y, hướng xuống tăng y, hướng trái giảm x, hướng phải tăng x
+	if (snake.dir == UP)
+		snake.dot[0].y--;
+	else if (snake.dir == DOWN)
+		snake.dot[0].y++;
+	else if (snake.dir == LEFT)
+		snake.dot[0].x--;
+	else if (snake.dir == RIGHT)
+		snake.dot[0].x++;
+}
+int Logic(Snake &snake, Fruit &fr, int &sleeptime)
+{
+	if (snake.dot[0].x == fr.fruit.x && snake.dot[0].y == fr.fruit.y)
+	{
+		for (int i=snake.n-1;i>0; i--)
+		{
+			snake.dot[i] = snake.dot[i - 1];
+		}
+		snake.n++;
+		//Theo hướng sẽ mọc đầu theo hướng đó
+		if (snake.dir == UP)
+			snake.dot[0].y--;
+		else if (snake.dir == DOWN)
+			snake.dot[0].y++;
+		else if (snake.dir == LEFT)
+			snake.dot[0].x--;
+		else if (snake.dir == RIGHT)
+			snake.dot[0].x++;
+
+		fr.fruit.x = rand() % width;
+		fr.fruit.y = rand() % height;
+
+		//Thời gian ăn hoa quả.
+		sleeptime -= 20;
+	}
+	if (snake.dot[0].x < 0 || snake.dot[0].x > width || snake.dot[0].y < 0 || snake.dot[0].y > height)
+	{
+		return -1;
+	}
+	for (int i = 1; i < snake.n; i++)
+	{
+		if (snake.dot[0].x == snake.dot[i].x && snake.dot[0].y == snake.dot[i].y)
+			return -1;
+	}
+}
+int main()
+{
+	//Khởi tạo bộ sinh số ngẫu nhiên
+	srand(time == (NULL));
+	int key = 0;
+	int sleeptime = 200;
+
+	Snake snake;
+	Fruit fr;
+	Initialize(snake,fr);
+	while (1)
+	{
+		//Hiển thị
+		Show(snake,fr);
+		//Điều khiển
+		Direction(snake);
+		//Xử lý
+		key = Logic(snake, fr,sleeptime);
+		if (key == -1)
+		{
+			gotoXY(width + 1, 10);
+			cout << "Gameover";
+			while (_getch() != 13);
+			break;
+		}
+
+		//Kết thúc game
+		Sleep(sleeptime);
+	}
+	
+	return 0;
+}
